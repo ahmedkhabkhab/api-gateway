49a7d3304be6267bea59d486a667b48e86b2e382
Ahmed Khabkhab <ahmed.khabkhab@gmail.com>
2023-01-08 11:47:13 +0100

    Add Redis

---
+++ src/main/java/com/bookshop/apigateway/config/RateLimiterConfig.java
@@ -0,0 +1,16 @@
+package com.bookshop.apigateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class RateLimiterConfig {
+
+    @Bean
+    public KeyResolver keyResolver() {
+        return exchange -> Mono.just("john_doe");
+    }
+
+}
