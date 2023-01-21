9a290c9d56d47d4b2b834874010fb357ec7ba905
Ahmed Khabkhab <ahmed.khabkhab@gmail.com>
2023-01-21 22:33:31 +0100

    Add SecurityConfig

---
+++ src/main/java/com/bookshop/apigateway/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.bookshop.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+
+}
