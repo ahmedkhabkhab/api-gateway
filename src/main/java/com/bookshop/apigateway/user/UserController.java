6909225524a30ad44a98c5af68af7342f9c34bfe
Ahmed Khabkhab <ahmed.khabkhab@gmail.com>
2023-01-26 08:18:01 +0100

    Inject AuthenticationPrincipal

--- src/main/java/com/bookshop/apigateway/user/UserController.java
+++ src/main/java/com/bookshop/apigateway/user/UserController.java
@@ -1,25 +1,26 @@
 package com.bookshop.apigateway.user;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 
+@RestController
 public class UserController {
 
     @GetMapping("user")
-    public Mono<User> getUser() {
-        return ReactiveSecurityContextHolder.getContext()
-                .map(SecurityContext::getAuthentication)
-                .map(auth -> (OidcUser)auth.getPrincipal())
-                .map(oidcUser -> new User(
-                        oidcUser.getPreferredUsername(),
-                        oidcUser.getName(),
-                        oidcUser.getFamilyName(),
-                        List.of("employee", "customer"))); // hardcoded for now, we'll update them later
+    public Mono<User> getUser(@AuthenticationPrincipal OidcUser oidcUser) {
+        var user = new User(
+                oidcUser.getPreferredUsername(),
+                oidcUser.getName(),
+                oidcUser.getFamilyName(),
+                List.of("employee", "customer")); // hardcoded for now, we'll update them later
+        return Mono.just(user);
     }
 
 }
