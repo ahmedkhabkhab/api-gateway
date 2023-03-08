7952a9b9b2938ba69f43589e5ef23b792a2aaae0
Ahmed Khabkhab <ahmed.khabkhab@gmail.com>
2023-03-08 17:57:53 +0100

    Allow static content and books list without auth and redirect unauth exceptions to 401 response

--- src/main/java/com/bookshop/apigateway/config/SecurityConfig.java
+++ src/main/java/com/bookshop/apigateway/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.bookshop.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
 @EnableWebFluxSecurity
@@ -16,7 +19,11 @@ public class SecurityConfig {
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http,
                                                      ReactiveClientRegistrationRepository clientRegistrationRepository) {
         return http
-                .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/", "/*.css", "/*.js", "/favicon.ico").permitAll() // Allow static content without authentication
+                        .pathMatchers(HttpMethod.GET, "/books/**").permitAll() // Allow retrieving books list without authentication
+                        .anyExchange().authenticated()) // Any other request requires authentication
+                .exceptionHandling(exceptionHandlingSpec -> exceptionHandlingSpec.authenticationEntryPoint(new HttpStatusServerEntryPoint((HttpStatus.UNAUTHORIZED)))) // Exceptions due to unauthorized user converted to HTTP 401 response
                 .oauth2Login(Customizer.withDefaults()) // Enable authentication with Oauth2/OpenID connect
                 .logout(logoutSpec -> logoutSpec.logoutSuccessHandler(oidcLogoutSuccessHandler(clientRegistrationRepository)))
                 .build();
