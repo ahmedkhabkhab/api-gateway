360bce11c79c9a45db5538839975ca90bbd016c0
Ahmed Khabkhab <ahmed.khabkhab@gmail.com>
2023-03-11 11:26:09 +0100

    Add test for UserController

---
+++ src/test/java/com/bookshop/apigateway/user/UserControllerTests.java
@@ -0,0 +1,62 @@
+package com.bookshop.apigateway.user;
+
+import com.bookshop.apigateway.config.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
+import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@WebFluxTest(UserController.class)
+@Import(SecurityConfig.class)
+public class UserControllerTests {
+
+    @Autowired
+    WebTestClient webClient;
+
+    @MockBean
+    ReactiveClientRegistrationRepository clientRegistrationRepository; // Used to skip interaction with Keycloak
+
+    @Test
+    void whenNotAuthenticatedThen401() {
+        webClient.get().uri("/user").exchange().expectStatus().isUnauthorized();
+    }
+
+    @Test
+    void whenAuthenticatedThenReturnUser() {
+        var expectedUser = new User(
+                "jon.snow",
+                "Jon",
+                "Snow",
+                List.of("employee", "customer"));
+
+        webClient.mutateWith(configureMockOidcLogin(expectedUser))
+                .get()
+                .uri("/user")
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody(User.class)
+                .value(user -> assertThat(user).isEqualTo(expectedUser));
+    }
+
+    /**
+     * Builds a mock ID Token
+     * @param expectedUser a user with the same information as the currently authenticated user
+     * @return a mock ID token
+     */
+    private SecurityMockServerConfigurers.OidcLoginMutator configureMockOidcLogin(User expectedUser) {
+        return SecurityMockServerConfigurers.mockOidcLogin().idToken(builder -> {
+            builder.claim(StandardClaimNames.PREFERRED_USERNAME, expectedUser.username());
+            builder.claim(StandardClaimNames.GIVEN_NAME, expectedUser.firstName());
+            builder.claim(StandardClaimNames.FAMILY_NAME, expectedUser.lastName());
+        });
+    }
+}
